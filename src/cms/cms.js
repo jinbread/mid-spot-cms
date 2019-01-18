ea1377c91a8d54fff126d969c14c7679c643b9ef
Jinbread <jinjae.lee@icloud.com>
2019-01-18 23:26:25 +0100

    add section test 9

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -2,10 +2,8 @@ import CMS from 'netlify-cms'
 
 import AboutPagePreview from './preview-templates/AboutPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
-import ExhibitionPostPreview from './preview-templates/ExhibitionPostPreview'
 import ProductPagePreview from './preview-templates/ProductPagePreview'
 
 CMS.registerPreviewTemplate('about', AboutPagePreview)
 CMS.registerPreviewTemplate('products', ProductPagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
-CMS.registerPreviewTemplate('exhibition', ExhibitionPostPreview)
