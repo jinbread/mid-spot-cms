0f30653ccf40a025dfbc467f760bff75560202dc
Jinbread <jinjae.lee@icloud.com>
2019-01-19 22:11:22 +0100

    style update test 1

--- src/pages/program/index.js
+++ src/pages/program/index.js
@@ -10,23 +10,18 @@ export default class ProgramPage extends React.Component {
 
     return (
       <Layout>
-        <section className="section">
-          <div className="container">
-            <h1>mid-spot</h1>
-            <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec et tempus nisi. Proin semper, magna non malesuada viverra, mauris leo placerat odio, eget rutrum velit nisi eu justo. Aliquam pharetra massa turpis, id dapibus velit efficitur eget. Nulla laoreet luctus ultrices. Integer porta massa sed orci pretium tristique. Nullam augue massa, fringilla ac magna ut, aliquet lacinia est. Vivamus fermentum neque justo, blandit tincidunt turpis varius vitae. Aliquam erat volutpat. Pellentesque ut condimentum erat, gravida malesuada diam. Praesent vel porttitor elit. Aenean vel porttitor ipsum, at pulvinar mi. Ut luctus egestas lectus ut venenatis.</div>
-          </div>
-        </section>
       
         <section className="section">
           <div className="container">
             <div className="content">
-              <h1 className="has-text-weight-bold is-size-2">program</h1>
+              <h1 className="has-text-weight-bold is-size-4">program</h1>
+              <hr/>
             </div>
             {posts
               .map(({ node: post }) => (
                 <div
                   className="content"
-                  style={{ border: '1px solid #333', padding: '2em 4em' }}
+                  style={{ padding: '2em 0' }}
                   key={post.id}
                 >
                   <p>
