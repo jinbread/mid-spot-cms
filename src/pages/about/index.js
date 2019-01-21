d40864c8de372830b213010b47b3cefab1925ef4
Jinbread <jinjae.lee@icloud.com>
2019-01-21 21:12:13 +0100

    Replaced markdown files in about and contact page to react files

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -2,14 +2,14 @@ import React from 'react'
 import { Link } from 'gatsby'
 import Layout from '../../components/Layout'
 
-export default class ContactPage extends React.Component {
+export default class AboutPage extends React.Component {
   render() {
     return (
       <Layout>
         <section className="section">
           <div className="container">
             <div className="content">
-              <h1 className="has-text-weight-bold is-size-4">contact</h1>
+              <h1 className="has-text-weight-bold is-size-4">about</h1>
               <hr/>
             </div>
           </div>
