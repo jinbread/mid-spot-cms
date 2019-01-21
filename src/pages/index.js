7f077d8a893f5f02e3176db468ecbe156491091f
Jinbread <jinjae.lee@icloud.com>
2019-01-21 20:54:55 +0100

    global css added

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Link, graphql } from 'gatsby'
 import Layout from '../components/Layout'
+import "../styles/global.css"
 
 export default class IndexPage extends React.Component {
   render() {
